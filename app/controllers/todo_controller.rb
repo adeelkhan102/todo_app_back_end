2e559e53a57624896919ae92590c03b352ecc83b
Adeel Khan <adeel.ulha@uasem.org>
2017-10-01 15:55:28 +0000

    update

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,23 +4,24 @@ class TodoController < ApplicationController
     end 
     
     def show 
-        if params[id]==1 
+  
+        if params[:id]=="1" 
             @task= "A.P English Speech paper"
             
     end
-       if params[id]==2
+       if params[:id]=="2"
            @task= "A.P Environmental Science summer hw"
      end
-       if params[id]==3
+       if params[:id]=="3"
            @task= "A.P U.S History Summer HW"
            
        end
-        if params[id]==4
+        if params[:id]=="4"
             @task= "A.P U.S History DBQ Essay"
             
         end
         
-         if params[id]==5
+         if params[:id]=="5"
              @task= "Trig. Packet"
          end
           
