56017ef42b9308f16a4ec1aa173d84688f5725a5
Adeel Khan <adeel.ulha@uasem.org>
2017-10-01 14:32:00 +0000

    update

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,15 +1,29 @@
 class TodoController < ApplicationController
- def index
-  @number=2
-  
- end
-  def show
-   if params[:id]=1
-    @tasks="make curriculum"
-    elsif params[:id]=2
-    @tasks="Complete"
-     
-   end
-  
- end 
-end
+    def index
+        
+    end 
+    
+    def show 
+        if params[id]==1 
+            @task= "A.P English Speech paper"
+            
+    end
+       if params[id]==2
+           @task= "A.P Environmental Science summer hw"
+     end
+       if params[id]==3
+           @task= "A.P U.S History Summer HW"
+           
+       end
+        if params[id]==4
+            @task= "A.P U.S History DBQ Essay"
+            
+        end
+        
+         if params[id]==5
+             @task= "Trig. Packet"
+         end
+          
+         end 
+        
+    end
